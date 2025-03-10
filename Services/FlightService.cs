8e3db55618275ad9f4b22ba4ca10c24b9d1c9739
janakhammash <janakhammash4@gmail.com>
2025-03-10 22:08:00 +0200

    Apply the Add/search flight methods

--- Services/FlightService.cs
+++ Services/FlightService.cs
@@ -1,6 +1,25 @@
 ﻿namespace Airport_Ticket_Booking_System.Services;
-
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Airport_Ticket_Booking_System.Database;
+using Airport_Ticket_Booking_System.Models;
 public class FlightService
 {
-    
+    private List<Flight> flights = FileHandler.LoadFlights();
+
+    public List<Flight> SearchFlights(string departureCountry, string destinationCountry, string flightClass)
+    {
+        return flights.Where(f =>
+            f.DepartureCountry.Equals(departureCountry, StringComparison.OrdinalIgnoreCase) &&
+            f.DestinationCountry.Equals(destinationCountry, StringComparison.OrdinalIgnoreCase) &&
+            f.Class.Equals(flightClass, StringComparison.OrdinalIgnoreCase)).ToList();
+    }
+
+    public void AddFlight(Flight flight)
+    {
+        flight.Id = flights.Count + 1;
+        flights.Add(flight);
+        FileHandler.SaveFlights(flights);
+    }
 }
