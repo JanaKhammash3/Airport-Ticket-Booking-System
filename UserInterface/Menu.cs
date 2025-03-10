19c39c75b5dcd261a18a0f6015449eff78fd060c
janakhammash <janakhammash4@gmail.com>
2025-03-10 21:57:30 +0200

    added Menu.cs

--- UserInterface/Menu.cs
+++ UserInterface/Menu.cs
@@ -1,6 +1,13 @@
-﻿namespace Airport_Ticket_Booking_System.UserInterface;
+﻿
+namespace Airport_Ticket_Booking_System.UserInterface;
 
 public class Menu
 {
-    
+    public static void ShowMainMenu()
+    {
+        Console.WriteLine("Airport Ticket Booking System");
+        Console.WriteLine("1. Passenger");
+        Console.WriteLine("2. Manager");
+        Console.WriteLine("3. Exit");
+    }
 }
