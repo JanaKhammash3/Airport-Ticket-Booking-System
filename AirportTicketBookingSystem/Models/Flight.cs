654e3ab032b87233153aa63d8f1673b7812e4d59
janakhammash <janakhammash4@gmail.com>
2025-04-13 01:39:19 +0200

    Fixed a nullable warning

--- AirportTicketBookingSystem/Models/Flight.cs
+++ AirportTicketBookingSystem/Models/Flight.cs
@@ -4,7 +4,7 @@ public class Flight
 {
     public int Id { get; set; }
     public decimal Price { get; set; }
-    public string DepartureCountry { get; set; }
+    public string DepartureCountry { get; set; } = string.Empty;
     public string DestinationCountry { get; set; }
     public DateTime DepartureDate { get; set; }
     public string DepartureAirport { get; set; }
