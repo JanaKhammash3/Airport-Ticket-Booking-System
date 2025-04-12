f81b8252bc5fcac6953d3b5cd10e6d3d0f80000a
janakhammash <janakhammash4@gmail.com>
2025-04-13 01:30:42 +0200

    Added FlightValidationTests.cs

---
+++ AirportTicketBookingSystem.Tests/FlightValidationTests.cs
@@ -0,0 +1,28 @@
+﻿using Airport_Ticket_Booking_System.Models;
+using Airport_Ticket_Booking_System.Services;
+using Xunit;
+
+namespace Airport_Ticket_Booking_System.AirportTicketBookingSystem.Tests;
+
+public class FlightValidationTests
+{
+    [Fact]
+    public void ValidateFlight_WithValidFlight_ReturnsValidMessage()
+    {
+        var flight = new Flight
+        {
+            DepartureCountry = "Germany",
+            DestinationCountry = "France",
+            DepartureAirport = "FRA",
+            ArrivalAirport = "CDG",
+            DepartureDate = DateTime.Today.AddDays(5),
+            Price = 300,
+            Class = "Economy"
+        };
+
+        var result = FlightValidation.ValidateFlight(flight);
+
+        Assert.Equal("Flight data is valid.", result);
+    }
+}
+
